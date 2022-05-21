1aeb172d4be5d5dad910f917f9077f6583d3f966
Sean <seanhung2000@gmail.com>
2022-05-21 12:58:23 +0800

    record fuction

--- js/app.js
+++ js/app.js
@@ -1,19 +1,11 @@
-var input;
-var rec;
 var mediaRecorder;
-
-window.AudioContext = window.AudioContext || window.webkitAudioContext;
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
-
-var AudioContext = window.AudioContext || window.webkitAudioContext;
-var audioContext; //  audio data
+var recordedChucks = [];
 
 var recordButton = document.getElementById("recordButton");
 var stopButton = document.getElementById("stopButton");
 var pauseButton = document.getElementById("pauseButton");
 
 $("#record-btn").on("click", () => {
-    $("#stop-btn").prop('disabled', false);
     startRecording();
 });
 $("#pause-btn").on("click", () => {
@@ -23,28 +15,24 @@ $("#stop-btn").on("click", () => {
     stopRecording()
 });
 
-var constraints = { audio: true, video:false }
-navigator.mediaDevices.getUserMedia({
-        audio: true
-    }).then(mediaStream => {
-        // console.log(mediaStream);
-
-        // audioContext = new AudioContext();
-        // $("#record-status").val("recording");
-        // input = audioContext.createMediaStreamSource(mediaStream);
-        // console.log("success")
-        mediaRecorder = new MediaRecorder(mediaStream);
-        console.log(mediaRecorder.state);
-    }).catch(function (error) {
-        console.log(error);
-});
-
 function startRecording() {
-    setMediaRecorderOnStop();
-    mediaRecorder.start();
-    $("#record-status").text("recording");
-    console.log(mediaRecorder.state);
-    console.log(mediaRecorder);
+    const constraints = { audio: true, video:false }
+    navigator.mediaDevices.getUserMedia({
+            audio: true
+        }).then(mediaStream => {
+            mediaRecorder = new MediaRecorder(mediaStream);
+            console.log(mediaRecorder.state);
+
+            setMediaRecorder();
+            mediaRecorder.start();
+
+            $("#record-status").text("recording");
+            $("#stop-btn").prop('disabled', false);
+            console.log(mediaRecorder.state);
+
+        }).catch(function (error) {
+            console.log(error);
+    });
 }
 
 function stopRecording() {
@@ -53,10 +41,19 @@ function stopRecording() {
     $("#record-status").text("stop");
 }
 
-function setMediaRecorderOnStop() {
-    mediaRecorder.onstop = function(e) {
-        console.log("recorder stopped");
-        var audioURL = window.URL.createObjectURL(e.data);
+function setMediaRecorder() {
+    mediaRecorder.ondataavailable = function(e) {
+        console.log(e);
+        if (e.data.size > 0) {
+            recordedChucks.push(e.data);
+        }
+        var blob = new Blob(recordedChucks, {
+            type: "video/webm"
+          });
+        var audioURL = URL.createObjectURL(blob);
+        // var audioURL = window.URL.createObjectURL(e.data);
+        console.log(audioURL);
         $("audio").attr("src", audioURL);
+        $("#stop-btn").prop('disable', true);
     }
 }
